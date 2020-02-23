88b9406e76e2aaa92e770b2c23b0ff8748e7f264
Jonathan Hernández <aggressivepixels@gmail.com>
2020-02-23 15:38:51 -0400

    Fix test

--- tests/Example.elm
+++ tests/Example.elm
@@ -8,10 +8,10 @@ import Test exposing (Test, describe, test)
 suite : Test
 suite =
     describe "Interpreter"
-        [ describe "fromProgram"
+        [ describe "init"
             [ test "fails with a program bigger than the available memory" <|
                 \_ ->
-                    Interpreter.fromProgram (List.repeat (4096 - 512 + 1) 0)
+                    Interpreter.init (List.repeat (4096 - 512 + 1) 0) 0
                         |> Expect.equal Nothing
             ]
         ]
